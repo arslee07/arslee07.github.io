2258aabd4d722710a2986881bcf721f05e8d3bf9
Arsenii Liunsha <50916030+arslee07@users.noreply.github.com>
2021-06-27 21:42:51 +0900

    removed useless junk i fucking planned to implemen

--- src/index.js
+++ src/index.js
@@ -1,13 +1,105 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import Main from './Pages/Main';
 import './Stylesheets/bootstrap.min.css';
-// import './index.css';
+import pfp from './Images/pfp.png';
+import { MDBAnimation, MDBCol, MDBRow } from 'mdbreact';
+import { FiGithub, FiMail, RiTelegramLine } from 'react-icons/all';
+import './Stylesheets/Header.css';
+import ReactTooltip from 'react-tooltip';
 
 render(
-  <HashRouter>
-    <Main />
-  </HashRouter>,
+  <>
+    <MDBAnimation type={'fadeIn'}>
+      <div
+        className={'col row justify-content-center align-items-center m-0'}
+        style={{
+          minHeight: '100vh',
+          padding: '5rem',
+        }}
+      >
+        <div className={'row text-center mx-auto justify-content-center'}>
+          <MDBCol className={'px-0'}>
+            <a href="/about">
+              <img
+                alt={'pfp'}
+                className="img-fluid shadow mx-auto my-auto pfp"
+                src={pfp}
+                style={{
+                  borderRadius: '50%',
+                  maxWidth: '120px',
+                  maxHeight: '120px',
+                }}
+              />
+            </a>
+          </MDBCol>
+          <MDBCol
+            middle
+            className={'text-sm-left text-center'}
+            style={{ minWidth: '200px' }}
+          >
+            <h1 className={'m-0'}>arslee 💻</h1>
+            <h3 className={'text-muted mx-0 mt-0 mb-2'}>i build things</h3>
+
+            <MDBRow around>
+              <a
+                data-tip
+                data-for={'github'}
+                href={'https://github.com/arslee07'}
+                style={{ color: '#211F1F' }}
+              >
+                <FiGithub fontSize={26} />
+              </a>
+              <ReactTooltip
+                place="bottom"
+                type="dark"
+                effect="solid"
+                id={'github'}
+                className={'py-1 px-2'}
+              >
+                <span>github.com/arslee07</span>
+              </ReactTooltip>
+
+              <a
+                data-tip
+                data-for={'telegram'}
+                href={'https://t.me/arslee'}
+                style={{ color: '#0088CC' }}
+              >
+                <RiTelegramLine fontSize={30} />
+              </a>
+              <ReactTooltip
+                place="bottom"
+                type="dark"
+                effect="solid"
+                id={'telegram'}
+                className={'py-1 px-2'}
+              >
+                <span>t.me/arslee</span>
+              </ReactTooltip>
+
+              <a
+                data-tip
+                data-for={'mail'}
+                href={'mailto:me@arslee.dev'}
+                style={{ color: '#EA4335' }}
+              >
+                <FiMail fontSize={26} />
+              </a>
+              <ReactTooltip
+                place="bottom"
+                type="dark"
+                effect="solid"
+                id={'mail'}
+                className={'py-1 px-2'}
+              >
+                <span>me@arslee.dev</span>
+              </ReactTooltip>
+            </MDBRow>
+          </MDBCol>
+        </div>
+      </div>
+    </MDBAnimation>
+  </>,
+
   document.getElementById('root')
 );
